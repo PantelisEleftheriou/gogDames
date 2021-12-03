31464e007a37269a3a20bffdd8c58954e23bac6d
unknown <admin@ADVANLAP308.ADVANTAGE.GR>
2021-12-03 23:10:06 +0200

    Initial commit

---
+++ src/main/java/gr/peoplecert/gogdames/GogDamesApplication.java
@@ -0,0 +1,13 @@
+package gr.peoplecert.gogdames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogDamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GogDamesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gr/peoplecert/gogdames/GogDamesApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.peoplecert.gogdames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GogDamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
