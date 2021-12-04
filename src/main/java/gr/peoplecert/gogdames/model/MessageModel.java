604b2d835ee52df2b257366976391f3058599fbc
IoanCode <a@ote.gr>
2021-12-04 22:55:30 +0100

    Message Model test

---
+++ src/main/java/gr/peoplecert/gogdames/model/MessageModel.java
@@ -0,0 +1,26 @@
+
+package gr.peoplecert.gogdames.model;
+
+public class MessageModel {
+   private String fromLogin;
+   private String message;
+
+    public String getFromLogin() {
+        return fromLogin;
+    }
+
+    public void setFromLogin(String fromLogin) {
+        this.fromLogin = fromLogin;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+   
+   
+    
+}
