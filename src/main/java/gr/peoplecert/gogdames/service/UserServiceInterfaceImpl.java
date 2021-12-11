02fe86d60e44d0b4ce75094c1566cda8b9c36ce2
IoanCode <a@ote.gr>
2021-12-11 13:03:59 +0100

    UserService Created addUser & getUserById methods

--- src/main/java/gr/peoplecert/gogdames/service/UserServiceInterfaceImpl.java
+++ src/main/java/gr/peoplecert/gogdames/service/UserServiceInterfaceImpl.java
@@ -2,7 +2,6 @@ package gr.peoplecert.gogdames.service;
 
 import gr.peoplecert.gogdames.model.User;
 import gr.peoplecert.gogdames.repository.UserRepositoryInterface;
-import java.util.Date;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
