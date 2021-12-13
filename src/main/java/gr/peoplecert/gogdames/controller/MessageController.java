6d730d0e14331a97fd66ef31adc31386866d9365
IoanCode <a@ote.gr>
2021-12-13 20:03:19 +0100

    message controller

--- src/main/java/gr/peoplecert/gogdames/controller/MessageController.java
+++ src/main/java/gr/peoplecert/gogdames/controller/MessageController.java
@@ -1,4 +1,3 @@
-
 package gr.peoplecert.gogdames.controller;
 
 import gr.peoplecert.gogdames.model.MessageModel;
@@ -13,20 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
-    
+
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
-    
+
     @Autowired
     private UserServiceInterface userServiceInterface;
-    
+
     @MessageMapping("/chat/{id}")
-    public void sendMessage(@DestinationVariable int id,MessageModel message){
+    public void sendMessage(@DestinationVariable int id, MessageModel message) {
         System.out.println("Send message: " + message + " to -> " + id);
         Optional<User> isUser = userServiceInterface.getUserById(id);
         if (isUser.isPresent()) {
-            simpMessagingTemplate.convertAndSend("/topic/messages" + id, message );         
-        }       
+            simpMessagingTemplate.convertAndSend("/topic/messages" + id, message);
+        }
     }
-    
+
 }
