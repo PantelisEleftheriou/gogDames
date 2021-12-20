73ace9c3c9b7775c23ce1c7108a1f75ccccbe8b2
ANNA <ANNA@DESKTOP-T1F03TM>
2021-12-20 14:10:02 +0200

    in progress

--- src/main/java/gr/peoplecert/gogdames/service/UserServiceInterfaceImpl.java
+++ src/main/java/gr/peoplecert/gogdames/service/UserServiceInterfaceImpl.java
@@ -1,18 +1,10 @@
 
 package gr.peoplecert.gogdames.service;
 
-import gr.peoplecert.gogdames.domain.User;
-import java.util.Date;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceInterfaceImpl implements UserServiceInterface {
+public class UserServiceInterfaceImpl  {
 
-    @Override
-    public User registerUser(String userName, String password, String email, String firstName, String lastName, Date dateOfBirth) {
-       User user = new User();
-       
-       return user;
-    }
-    
+   
 }
