59c31a3c3849e8acf3aab76a60414b6fa933f8ba
Ioannis Chloptsios <i.chloptsios@stoics-it.com>
2021-12-23 17:59:36 +0200

    6

--- src/main/java/gr/peoplecert/gogdames/service/UserServiceInterfaceImpl.java
+++ src/main/java/gr/peoplecert/gogdames/service/UserServiceInterfaceImpl.java
@@ -9,8 +9,11 @@ import java.util.Optional;
 
 import net.bytebuddy.utility.RandomString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
