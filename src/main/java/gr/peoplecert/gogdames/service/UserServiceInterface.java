f2de062d40af00a5690017351d9ee192036669ad
Ioannis Chloptsios <i.chloptsios@stoics-it.com>
2021-12-23 17:58:10 +0200

    5

--- src/main/java/gr/peoplecert/gogdames/service/UserServiceInterface.java
+++ src/main/java/gr/peoplecert/gogdames/service/UserServiceInterface.java
@@ -2,15 +2,24 @@
 package gr.peoplecert.gogdames.service;
 
 import gr.peoplecert.gogdames.model.User;
-import java.util.Date;
+
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Optional;
 
 
-public interface UserServiceInterface  {
-    
+public interface UserServiceInterface {
+
     User addUser(User user);
+
     Optional<User> getUserById(int id);
+
+    User registerUser(User user);
+
+    void sendVerificationEmail(User user, String siteUrl) throws MessagingException, UnsupportedEncodingException;
+
     List<User> findAll();
+
     void deleteUser(int userId);
 }
--- src/main/java/gr/peoplecert/gogdames/service/UserServiceInterfaceImpl.java
+++ src/main/java/gr/peoplecert/gogdames/service/UserServiceInterfaceImpl.java
@@ -2,16 +2,32 @@ package gr.peoplecert.gogdames.service;
 
 import gr.peoplecert.gogdames.model.User;
 import gr.peoplecert.gogdames.repository.UserRepositoryInterface;
+
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Optional;
+
+import net.bytebuddy.utility.RandomString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class UserServiceInterfaceImpl implements UserServiceInterface {
 
     @Autowired
     UserRepositoryInterface userRepositoryInterface;
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public User addUser(User user) {
@@ -30,6 +46,41 @@ public class UserServiceInterfaceImpl implements UserServiceInterface {
             return Optional.empty();
         }
     }
+
+
+    @Override
+    public User registerUser(User user) {
+//        passwordEncoder(user);
+        user.setRegistered(false);
+
+        String randomCode = RandomString.make(64);
+        user.setVerificationCode(randomCode);
+
+        return  userRepositoryInterface.save(user);
+    }
+
+    @Override
+    public void sendVerificationEmail(User user, String siteUrl) throws MessagingException, UnsupportedEncodingException {
+        String subject = "Please Verify your Registration";
+        String senderName = "gogDames games";
+        String mailContent = "<p>Dear" + user.getUsername() + ",</p>";
+        mailContent += "<p>Please click the link below to verify your registration:</p>";
+
+        String verifyUrl = siteUrl + "/verify?code=" + user.getVerificationCode();
+        mailContent += "<a href=\"" + verifyUrl + "\">VERIFY</a>";
+
+        mailContent += "<p>Thank you</p>";
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setFrom("proj.avengers@gmail.com", senderName);
+        helper.setTo(user.getEmail());
+        helper.setSubject(subject);
+        helper.setText(mailContent, true);
+        mailSender.send(message);
+    }
+
 
     @Override
     public List<User> findAll() {
