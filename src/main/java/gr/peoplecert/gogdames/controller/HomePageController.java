357ab379946b92ab8103d5d829f542508df82111
Ioannis Chloptsios <i.chloptsios@stoics-it.com>
2021-12-23 17:31:52 +0200

    3

--- src/main/java/gr/peoplecert/gogdames/controller/HomePageController.java
+++ src/main/java/gr/peoplecert/gogdames/controller/HomePageController.java
@@ -1,8 +1,13 @@
 package gr.peoplecert.gogdames.controller;
 
+import gr.peoplecert.gogdames.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/")
@@ -11,6 +16,18 @@ public class HomePageController {
     @GetMapping("/login")
     public String showHomePage() {
         return "login";
+    }
+
+    @PostMapping("/VerifyUser")
+    public String registerUser(User user, Model model, HttpServletRequest request) throws MessagingException, UnsupportedEncodingException {
+        service.registerUser(user);
+
+        String siteUrl = Utility.getSiteUrl(request);
+        service.sendVerificationEmail(user, siteUrl);
+
+        model.addAttribute("page", "You need to verify your account");
+
+        return "verify";
     }
 
 }
